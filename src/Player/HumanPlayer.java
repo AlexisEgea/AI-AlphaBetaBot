c4ce13b0e08004dba847c66515bbcbbe4d53eff0
AlexisEgea <alexisegea@outlook.com>
2025-02-12 02:23:10 +0100

    Adding user class

---
+++ src/Player/HumanPlayer.java
@@ -0,0 +1,25 @@
+package Player;
+
+import java.util.Scanner;
+
+public class HumanPlayer extends Player {
+    private Scanner scanner;
+
+    public HumanPlayer(int playerId) {
+        super(playerId);
+        this.scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public int decide() {
+        System.out.print("Your action: ");
+
+        while (!scanner.hasNextInt()) {
+            System.out.println("Error Wrong Action: Column integer needed.");
+            System.out.print("Your action: ");
+            scanner.next();
+        }
+
+        return scanner.nextInt();
+    }
+}
