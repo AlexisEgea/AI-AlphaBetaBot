d4e54fda78c054f34ec125c079560c2400d80db4
AlexisEgea <alexisegea@outlook.com>
2025-02-14 18:08:28 +0100

    Deleting PlayAction for two players

--- src/MVC/Controller/Controller.java
+++ src/MVC/Controller/Controller.java
@@ -2,25 +2,17 @@ package MVC.Controller;
 
 import Game.Game;
 import MVC.View.View;
-import Player.Player;
 import Player.HumanPlayer;
 
 import java.util.ArrayList;
 
 public class Controller {
     private Game game;
-//    private View view;
-    private Player player1;
-    private Player player2;
-    private Player currentPlayer;
 
     private ArrayList<View> observers;
 
-    public Controller(Game game, Player player1, Player player2) {
+    public Controller(Game game) {
         this.game = game;
-        this.player1 = player1;
-        this.player2 = player2;
-        this.currentPlayer = player1;
 
         this.observers = new ArrayList<>();
 
@@ -30,17 +22,6 @@ public class Controller {
         return game;
     }
 
-    public Player getPlayer1() {
-        return player1;
-    }
-
-    public Player getPlayer2() {
-        return player2;
-    }
-
-    public Player getCurrentPlayer() {
-        return currentPlayer;
-    }
 
     public void addObserver(View observer){
         this.observers.add(observer);
@@ -62,50 +43,31 @@ public class Controller {
         }
     }
 
-    public void playAction(int column) {
-        this.playHumanMove(column);
-        if (this.game.endGame(new Player[]{this.player1, this.player2}))
-            this.notifyEndGame();
-        else{
-            this.playBotMove();
-            System.out.println("DEBUG: Yellow player has played.");
-            if (this.game.endGame(new Player[]{this.player1, this.player2}))
-                this.notifyEndGame();
-        }
-    }
-
-        public void playHumanMove(int column) {
-        if(currentPlayer instanceof HumanPlayer){
-            boolean goodAction = game.playAction(currentPlayer, column);
-            if (goodAction){
-                this.switchPlayer();
+    public void playHumanMove(int column) {
+        if(this.game.getCurrentPlayer() instanceof HumanPlayer) {
+            boolean goodAction = game.playAction(this.game.getCurrentPlayer(), column);
+            if (goodAction) {
+                this.getGame().switchCurrentPlayer();
                 this.notifyObservers();
-
+            }
+            if (this.game.endGame()){
+                this.notifyEndGame();
             }
         }
-
     }
 
     public void playBotMove() {
-        if(!(currentPlayer instanceof HumanPlayer)){
-            int action = currentPlayer.decide();
-            boolean goodAction = game.playAction(currentPlayer, action);
+        if(!(this.game.getCurrentPlayer() instanceof HumanPlayer)){
+            int action = this.game.getCurrentPlayer().decide();
+            boolean goodAction = game.playAction(this.getGame().getCurrentPlayer(), action);
             if (goodAction) {
-                this.switchPlayer();
+                this.getGame().switchCurrentPlayer();
                 this.notifyObservers();
             }
+            if (this.game.endGame()){
+                this.notifyEndGame();
+            }
         }
-        if (this.game.endGame(new Player[]{this.player1, this.player2}))
-            this.notifyEndGame();
-    }
-
-    private void switchPlayer() {
-        int currentPlayerId = (currentPlayer.getPlayerId() + 1) % 2;
-        if(currentPlayerId == player1.getPlayerId())
-            currentPlayer = player1;
-        else
-            currentPlayer = player2;
-
     }
 
 }
