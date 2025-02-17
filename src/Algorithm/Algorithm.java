7cd59edc0f968f9026d42af9546bf9cb45f0271f
AlexisEgea <alexisegea@outlook.com>
2025-02-17 02:14:33 +0100

    Adding comments

--- src/Algorithm/Algorithm.java
+++ src/Algorithm/Algorithm.java
@@ -4,5 +4,12 @@ import Game.Game;
 import Player.Player;
 
 public interface Algorithm {
+    /**
+     * This method aims to use an algorithm to determine the best action to win the game.
+     *
+     * @param game   The game in which the action is performed
+     * @param player The player making the action
+     * @return The best action to play
+     */
     int bestAction(Game game, Player player);
 }
